b93733b967e53031b20d97a3d72c3490e115bc11
compiler <lukerochedev@gmail.com>
2021-03-15 19:16:47 -0400

    cmake build done

--- Waffles/src/Waffles/Tools/FileUtils/FileLoader.h
+++ Waffles/src/Waffles/Tools/FileUtils/FileLoader.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <fstream>
 
-#define WAFFLES_INTERNAL(x)         "src/Resources/" x
+#define WAFFLES_INTERNAL(x)         "../Waffles/src/Resources/" x
 #define WAFFLES_INTERNAL_SHADER(x)  WAFFLES_INTERNAL("Shaders/") x
 #define WAFFLES_INTERNAL_SHADER_SPIRV(x)  WAFFLES_INTERNAL("Shader_SPIRV/") x
 #define WAFFLES_INTERNAL_TEXTURE(x) WAFFLES_INTERNAL("Textures/") x
