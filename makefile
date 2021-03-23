b4bbe27413f015c4870a391624896a5142fa8fdf
compiler <lukerochedev@gmail.com>
2021-03-23 14:57:30 -0400

    build tools

--- makefile
+++ makefile
@@ -1,4 +1,13 @@
 all: main
 
 main:
+	build.bat
+
+launch:
+	build_run.bat
+
+run:
 	run.bat
+
+clean:
+	clean.bat
--- run.bat
+++ run.bat
@@ -1,5 +1,2 @@
-if not exist "build" mkdir build
 cd build
-cmake -G "MinGW Makefiles" ..
-make
 WafflesDriver.exe
